704862fed398e9feac642679bcf8e5608254dff6
Sheraz Ahmad <sheraz22@my.yorku.ca>
2026-02-06 20:38:00 -0500

    Completed XML file and DetailAcitivity File

--- app/src/main/java/com/example/eecs4443lab2/DetailActivity.java
+++ app/src/main/java/com/example/eecs4443lab2/DetailActivity.java
@@ -2,8 +2,8 @@ package com.example.eecs4443lab2;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,37 +11,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button itemButton = findViewById(R.id.itemButton);
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        Intent intent = getIntent();
+        TextView descriptionBox = findViewById(R.id.descriptionBox);
+        TextView titleBox = findViewById(R.id.titleBox);
+        ImageView thumbnail = findViewById(R.id.thumbnail);
+
+        descriptionBox.setText(intent.getStringExtra("description"));
+        titleBox.setText(intent.getStringExtra("title"));
+        thumbnail.setImageResource(intent.getIntExtra("imageResId", 0));
 
 
-        itemButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                detailedScreen(v, item);
-            }
-        });
-    }
-
-    public void detailedScreen(View v, Item item)
-    {
-        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
-        intent.putExtra("title", item.getTitle());
-        intent.putExtra("imageResId", item.imageResId());
-        intent.putExtra("description", item.getDescription());
-        startActivity(intent);
     }
 }
